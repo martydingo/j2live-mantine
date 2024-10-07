18e9b7efc55c2b2c7aff7ba510ba046352c426f6
Martin George <marty@dingo.foo>
2024-10-07 09:38:01 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,48 @@
 import "@mantine/core/styles.css";
 import { AppShell, Code, Divider, Flex, MantineProvider, Textarea, Title } from "@mantine/core";
 import FileIconsJinja from '~icons/file-icons/jinja';
+import { useState } from "react";
 import { theme } from "./theme";
 
+async function renderTemplate(yamlVariables: string, jinja2Template: string) {
+  try {
+      const postRequest = await fetch("http://localhost:8000/", {
+          headers: { "Content-Type": "application/json" },
+          method: "POST",
+          body: JSON.stringify({
+              "YAML": yamlVariables,
+              "Jinja": jinja2Template
+          }),
+          mode: 'cors',
+      })
+      const postResponse = await postRequest.json()
+      return JSON.parse(JSON.stringify(postResponse['message']))
+
+  }
+  catch (err) { return err }
+}
+
 export default function App() {
+  const [formData, setFormData] = useState({
+    yamlVariables: "x:\n  - name: abc\n  - name: def",
+    jinjaTemplate: "{% for y in x %}\n{{ y }}\n{% endfor %}",
+    generatedOutput: ""
+});
+
+const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+    let generatedOutput
+    if (name === "yamlVariables") {
+        generatedOutput = renderTemplate(value, formData.jinjaTemplate)
+    } else {
+        generatedOutput = renderTemplate(formData.yamlVariables, value)
+    }
+    Promise.all([generatedOutput]).then((output) => {
+        setFormData((prevState) => ({ ...prevState, generatedOutput: output as unknown as string }))
+    })
+  }
+
   return <MantineProvider theme={theme} defaultColorScheme="dark">
     <AppShell
       header={{ height: "2em" }}
@@ -32,7 +71,8 @@ export default function App() {
             style={{ width: "100%", height: "100%" }}
           >
             <Textarea
-              label="YAML"
+              label="YAML Variables"
+              description="e.g. some_var: abc"
               minRows={21}
               maxRows={21}
               autosize
@@ -41,7 +81,8 @@ export default function App() {
               style={{ marginTop: "2em", marginBottom: "1em" }}
             />
             <Textarea
-              label="Jinja2"
+              label="Jinja2 Template"
+              description="e.g. {{ some_var }}"
               minRows={20}
               maxRows={20}
               autosize
