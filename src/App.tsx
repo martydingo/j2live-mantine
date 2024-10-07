2169b57bfe0b386cabfc900c21aac638a315ce1a
Martin George <marty@dingo.foo>
2024-10-07 11:23:50 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,62 @@
 import "@mantine/core/styles.css";
-import { AppShell, Code, Divider, Flex, MantineProvider, Textarea, Title } from "@mantine/core";
+import { AppShell, Code, Divider, Flex, MantineProvider, Text, Textarea, Title, TypographyStylesProvider } from "@mantine/core";
 import FileIconsJinja from '~icons/file-icons/jinja';
 import { useState } from "react";
 import { theme } from "./theme";
 
 async function renderTemplate(yamlVariables: string, jinja2Template: string) {
   try {
-      const postRequest = await fetch("http://localhost:8000/", {
-          headers: { "Content-Type": "application/json" },
-          method: "POST",
-          body: JSON.stringify({
-              "YAML": yamlVariables,
-              "Jinja": jinja2Template
-          }),
-          mode: 'cors',
-      })
-      const postResponse = await postRequest.json()
-      return JSON.parse(JSON.stringify(postResponse['message']))
+    const postRequest = await fetch("http://localhost:8000/", {
+      headers: { "Content-Type": "application/json" },
+      method: "POST",
+      body: JSON.stringify({
+        "YAML": yamlVariables,
+        "Jinja": jinja2Template
+      }),
+      mode: 'cors',
+    })
+    const postResponse = await postRequest.json()
+    return {
+      error: postResponse['error'],
+      message: JSON.parse(JSON.stringify(postResponse['message']))
+    }
 
   }
-  catch (err) { return err }
+  catch (err) {
+    return {
+      error: true,
+      message: err
+    }
+  }
 }
 
 export default function App() {
   const [formData, setFormData] = useState({
-    yamlVariables: "x:\n  - name: abc\n  - name: def",
-    jinjaTemplate: "{% for y in x %}\n{{ y }}\n{% endfor %}",
-    generatedOutput: ""
-});
+    yamlVariables: "",
+    jinja2Template: "",
+  });
+  const [yamlErrorState, setYamlErrorState] = useState();
+  const [jinja2ErrorState, setJinja2ErrorState] = useState();
 
-const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const { name, value } = event.currentTarget;
+    const curElement = document.getElementById(name)
+    const outputElement = document.getElementById("generatedOutput")
     let generatedOutput
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
     if (name === "yamlVariables") {
-        generatedOutput = renderTemplate(value, formData.jinjaTemplate)
+      generatedOutput = renderTemplate(value, formData.jinja2Template)
     } else {
-        generatedOutput = renderTemplate(formData.yamlVariables, value)
+      generatedOutput = renderTemplate(formData.yamlVariables, value)
     }
     Promise.all([generatedOutput]).then((output) => {
-        setFormData((prevState) => ({ ...prevState, generatedOutput: output as unknown as string }))
+      if (name === "yamlVariables") {
+        setYamlErrorState(output[0].error as unknown as string)
+      } else {
+        setJinja2ErrorState(output[0].error as unknown as string)
+      }
+      setFormData((prevState) => ({ ...prevState, generatedOutput: output[0].message as unknown as string }))
+      outputElement!.innerText = output[0].message as unknown as string
     })
   }
 
@@ -51,63 +68,61 @@ const handleChange = (e) => {
     >
       <AppShell.Header>
         <Flex
-          align="center"
+          align="start"
           gap="xs"
-          style={{ marginLeft: "1em", marginTop: "1em", marginBottom: "0.3em" }}
+          style={{ marginLeft: "1.5em", marginTop: "1em" }}
         >
-          <FileIconsJinja style={{ fontSize: '2.5em' }} />
-          <Title order={2}>J2Live</Title>
+          <FileIconsJinja style={{ fontSize: '2.1em' }} />
+          <Title>J2Live</Title>
         </Flex>
       </AppShell.Header>
-      <AppShell.Main
-        style={{ marginTop: "1em" }}
-      >
-        <Flex
-          direction="row"
-          justify="center"
-        >
+      <AppShell.Main>
+        <TypographyStylesProvider>
           <Flex
-            direction="column"
-            style={{ width: "100%", height: "100%" }}
+            direction="row"
+            justify="center"
           >
-            <Textarea
-              label="YAML Variables"
-              description="e.g. some_var: abc"
-              minRows={21}
-              maxRows={21}
-              autosize
-            />
-            <Divider
-              style={{ marginTop: "2em", marginBottom: "1em" }}
-            />
-            <Textarea
-              label="Jinja2 Template"
-              description="e.g. {{ some_var }}"
-              minRows={20}
-              maxRows={20}
-              autosize
-            />
+            <Flex
+              direction="column"
+              style={{ width: "100%", height: "100%", padding: "1em", maxWidth: "45vw", maxHeight: "45vh" }}
+            >
+              <Textarea
+                label="YAML Variables"
+                name="yamlVariables"
+                id="yamlVariables"
+                description="e.g. some_var: abc"
+                minRows={21}
+                maxRows={21}
+                autosize
+                onChange={(event) => handleChange(event)}
+                value={formData.yamlVariables}
+                error={yamlErrorState || false}
+              />
+              <Textarea
+                label="Jinja2 Template"
+                name="jinja2Template"
+                id="jinja2Template"
+                description="e.g. {{ some_var }}"
+                minRows={20}
+                maxRows={20}
+                autosize
+                onChange={(event) => handleChange(event)}
+                value={formData.jinja2Template}
+                error={jinja2ErrorState}
+                style={{ marginTop: "1em" }}
+              />
+            </Flex>
+            <Flex
+              direction="column"
+              style={{ width: "100%", height: "100%", padding: "1em", marginTop: "3.6em", maxWidth: "45vw" }}
+            >
+              <Code id="generatedOutput"
+                style={{ textWrap: "pretty", height: "100%" }}
+                block />
+            </Flex>
           </Flex>
-          <Flex
-            direction="column"
-            style={{ width: "100%", height: "100%" }}
-          >
-            <Code block>
-              Blah
-            </Code>
-          </Flex>
-        </Flex>
+        </TypographyStylesProvider>
       </AppShell.Main>
-      <AppShell.Footer
-        style={{ marginBottom: "1em" }}
-      >
-        <Flex
-          align="center"
-          justify="center"
-        >
-          J2Live v0.1
-        </Flex>
-      </AppShell.Footer>
     </AppShell>
   </MantineProvider>;
 }
